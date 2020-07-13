853d9762cd729c0ab3b588071fa45b69eb0c1459
Vishal <vishal1998@gmail.com>
2020-07-13 02:15:01 -0700

    Fix lint issues and add comments for google auth

--- snapshot.py
+++ snapshot.py
@@ -6,7 +6,6 @@ except ImportError:
 import os
 from fbchat import Client
 from fbchat.models import Message, ThreadType
-from pathlib import Path
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from pydrive.files import GoogleDriveFile
@@ -17,19 +16,20 @@ FB_USERNAME = os.environ.get("FB_USERNAME")
 FB_PASSWORD = os.environ.get("FB_PASSWORD")
 FB_GROUP_ID = os.environ.get("FB_GROUP_ID")
 
+
 def lambda_handler(event, context):
     post_to_group()
-    return {'message' : event.get("message", "Hi. No message provided.")}
+    return {"message": event.get("message", "Hi. No message provided.")}
+
 
 def post_to_group() -> None:
-    """ Sends image to facebook group chat. 
+    """ Sends image to facebook group chat.
 
-        Location is demarcated by `thread_id`. 
+        Location is demarcated by `thread_id`.
         Scheduled to run every two days at 12:30 PM.
     """
     img_file = get_image()
-    
-    print(FB_USERNAME, FB_PASSWORD)
+
     client = Client(FB_USERNAME, FB_PASSWORD)
     if not client.isLoggedIn():
         client.login(FB_USERNAME, FB_PASSWORD)
@@ -39,24 +39,30 @@ def post_to_group() -> None:
     img_file.GetContentFile(img_path)
     img_message = Message(text="Sent from Lambda.")
     client.sendLocalImage(
-        img_path, 
-        message=img_message, 
-        thread_id=FB_GROUP_ID, 
+        img_path,
+        message=img_message,
+        thread_id=FB_GROUP_ID,
         thread_type=ThreadType.GROUP,
     )
     clean_up(img_path, client)
 
+
 def get_image() -> GoogleDriveFile:
-    """ Gets random image from Google Drive folder. 
-    
+    """ Gets random image from Google Drive folder.
+
         Silenty logs into Google Drive using local client secrets
         or locally stored credentials. Downloads file to directory.
         Configuration details stored in `settings.yaml`.
 
         Returns:
-            img_file: Google Drive File instance. 
-    """ 
+            img_file: Google Drive File instance.
+    """
+    # Authorized using local `client_secrets.json`, which can be
+    # downloaded from Google's API projects page, accessed through
+    # `settings.yaml`
     gauth = GoogleAuth()
+    # Uses local `credentials.json` to provide stored credentials
+    # to bypass verification proccess.
     gauth.LocalWebserverAuth()
     drive = GoogleDrive(gauth)
 
@@ -65,6 +71,7 @@ def get_image() -> GoogleDriveFile:
     rand_int = randrange(len(file_list))
     img_file = file_list[rand_int]
     return img_file
+
 
 def clean_up(img_path: str, client: Client) -> None:
     """ Removes local created image and logs out of facebook. """
